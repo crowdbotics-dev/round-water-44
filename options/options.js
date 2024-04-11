2d2bc529ee3e1018a785d2464a397fc46a5958c9
Crowdbotics <engineering@crowdbotics.com>
2024-04-11 15:29:47 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "round-water-44",
+  url: "https://round-water-44.botics.co",
+  api: "https://round-water-44.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
